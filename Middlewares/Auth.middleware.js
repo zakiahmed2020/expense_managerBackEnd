6cf049a5862eb603d6925d93007a65c605914c32
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-02 17:38:16 +0300

    middlewares

---
+++ Middlewares/Auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+function Auth(req, res, next) {
+  const token = req.headers["authorization"];
+  console.log(req.url);
+  if (!token)
+    return res
+      .status(401)
+      .send({ message: "Access Denied. No token Provided." });
+  try {
+    const tokn = token.slice(7);
+    jwt.verify(tokn, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).send({
+          message: "InvalidToken",
+        });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } catch (error) {
+    res.status(401).send({ message: `Error: ${error}` });
+  }
+}
+
+module.exports = Auth;
