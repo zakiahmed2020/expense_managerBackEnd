ed61e0960abdec2156b7987b1e444e945fdf6520
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-13 21:34:33 +0300

    Fixed Bug On Mongo URL

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const DB = "expenseManager";
 async function connectedToDB() {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/${DB}`);
+    await mongoose.connect(`mongodb://127.0.0.1:27017/${DB}`);
     console.log("Connected to MongoDB .....");
   } catch (e) {
     console.error(`Failed To connect to MongoDB....`);
