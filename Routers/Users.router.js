447e3d93bd619619847ade4935bf6f851a5de49d
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-27 09:50:36 +0300

    User.Router add Validations

--- Routers/Users.router.js
+++ Routers/Users.router.js
@@ -5,7 +5,11 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 // const auth = require("../Middlewares/Auth.middleware");
-const { UserModel, validateUsers } = require("../Models/Users.model");
+const {
+  UserModel,
+  validateUsers,
+  updateValidation,
+} = require("../Models/Users.model");
 const upload = require("../Utils/Multer");
 
 router.get("/me", async function (req, res) {
@@ -53,54 +57,61 @@ router.get("/:id", async function (req, res) {
 
 router.post("/create-user", upload.single("avatar"), async function (req, res) {
   const { error } = validateUsers(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(406).send({ message: error.details[0].message });
   const checkUsername = await UserModel.findOne({
     username: req.body.username,
   });
-  
+  if (checkUsername)
+    return res.send({
+      message: `username: ${checkUsername.username} is already exists...`,
+    });
+
   // get avatar in file
   const avatar = req.file ? req.file.path : "uploads\\default.png";
 
-  if (checkUsername)
-    return res.send({
-      message: "This username alreday exists..",
-      status: false,
-    });
   try {
     let AccountNo = 0;
     let [respnse] = await UserModel.find({}).sort({ createdAt: -1 }).limit(1);
     if (respnse) {
-      console.log(respnse);
       AccountNo = respnse.accountNo + 1;
     } else {
       AccountNo = 3000;
     }
 
     const userinfo = new UserModel({
+      avatar: avatar,
       name: req.body.name,
+      email: req.body.email,
       phone: req.body.phone,
       username: req.body.username,
-      pin: req.body.pin,
+      password: req.body.password,
       accountNo: AccountNo,
-      avatar: avatar,
     });
     const salt = await bcrypt.genSalt(10);
-    userinfo.pin = await bcrypt.hash(userinfo.pin, salt);
+    userinfo.password = await bcrypt.hash(userinfo.password, salt);
     const result = await userinfo.save();
+
+    let sendData = { ...result._doc };
+    delete sendData.password;
+    delete sendData.__v;
+
     res.send({
-      message: "User Registered Successfully",
       status: 200,
-      result: result,
+      message: "User Registered Successfully",
+      userInfo: sendData,
     });
-  } catch (err) {
-    console.error(err);
+  } catch (er) {
+    res.send({
+      status: 400,
+      message: `Error: ${er}`,
+    });
   }
 });
 
 router.delete("/:id", async function (req, res) {
   try {
-    const expenseInfo = await UserModel.findByIdAndRemove(req.params.id);
-    if (!expenseInfo) return res.status(400).send("ID was not found");
+    const userInfo = await UserModel.findByIdAndRemove(req.params.id);
+    if (!userInfo) return res.status(404).send("ID was not found");
     res.send({
       status: 200,
       message: "Seccessfully Deleted",
@@ -132,7 +143,7 @@ router.put("/passUpdate", async function (req, res) {
   const pcript_pin = await bcrypt.hash(pin, salt);
   // res.send(pcript_pin)
 
-  const pinUpdate = await UserModel.findByIdAndUpdate(
+  const _ = await UserModel.findByIdAndUpdate(
     _id,
     {
       pin: pcript_pin,
@@ -145,13 +156,24 @@ router.put("/passUpdate", async function (req, res) {
   });
 });
 
-router.put("/update-user/:id", upload.single("avatar"), async (req, res) => {
-  const { error } = validateUsers(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+// updating user info without password
+router.put("/update-userinfo/:id", async (req, res) => {
+  const { error } = updateValidation(req.body);
+  if (error)
+    return res.status(404).send({ message: `${error.details[0].message}` });
   try {
-    console.log(req.body);
     const id = await UserModel.findById(req.params.id);
-    if (!id) return res.status(400).send({ message: "ID was not found" });
+    if (!id) return res.status(404).send({ message: "Not Found" });
+
+    // const checkUsername = await UserModel.findOne({
+    //   username: req.body.username,
+    // }).lean();
+
+    // if (checkUsername) {
+    //   res.send({
+    //     message: `username: ${checkUsername.username} is already exists...`,
+    //   });
+    // } else {
 
     // get avatar in file. check the file has path.
     const avatar = req.file ? req.file.path : "uploads\\default.png";
@@ -161,34 +183,76 @@ router.put("/update-user/:id", upload.single("avatar"), async (req, res) => {
       fs.unlink(id.avatar, function (err) {
         if (err) return console.log(err);
         // if no error, file has been deleted successfully
-        // console.log("File deleted!");
+        console.log("File deleted!");
       });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashPass = bcrypt.hashSync(req.body.password, salt);
-    console.log(hashPass);
-    const updateUser = await UserModel.findByIdAndUpdate(
+    const _ = await UserModel.findByIdAndUpdate(
       req.params.id,
       {
+        avatar: avatar,
         name: req.body.name,
+        email: req.body.email,
         phone: req.body.phone,
         username: req.body.username,
-        password: hashPass,
-        avatar: avatar,
       },
       { new: true }
     );
     res.send({
       status: 200,
-      message: "Successfully Updated",
-      updateUser: updateUser,
+      message: "Updated Successfully",
     });
+    // }
   } catch (e) {
-    res.send({
-      status: 400,
-      message: `Error: ${e}`,
-    });
+    res.send({ status: 404, message: `Error: ${e}` });
   }
 });
+
+// router.put("/update-user/:id", upload.single("avatar"), async (req, res) => {
+//   const { error } = validateUsers(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+//   try {
+//     // console.log(req.body);
+//     const id = await UserModel.findById(req.params.id);
+//     if (!id) return res.status(400).send({ message: "ID was not found" });
+
+//     // get avatar in file. check the file has path.
+//     const avatar = req.file ? req.file.path : "uploads\\default.png";
+
+//     // delete old image. if is not equal default.
+//     if (id.avatar !== "uploads\\default.png") {
+//       fs.unlink(id.avatar, function (err) {
+//         if (err) return console.log(err);
+//         // if no error, file has been deleted successfully
+//         // console.log("File deleted!");
+//       });
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashPass = bcrypt.hashSync(req.body.password, salt);
+//     console.log(hashPass);
+//     const updateUser = await UserModel.findByIdAndUpdate(
+//       req.params.id,
+//       {
+//         name: req.body.name,
+//         phone: req.body.phone,
+//         username: req.body.username,
+//         password: hashPass,
+//         avatar: avatar,
+//       },
+//       { new: true }
+//     );
+//     res.send({
+//       status: 200,
+//       message: "Successfully Updated",
+//       updateUser: updateUser,
+//     });
+//   } catch (e) {
+//     res.send({
+//       status: 400,
+//       message: `Error: ${e}`,
+//     });
+//   }
+// });
+
 module.exports = router;
