b0149ce95bc5cc216419241be08ddee21f6b5c99
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-27 09:48:45 +0300

    Middleware - admin.middleware deprecated

--- Middlewares/Admin.middleware.js
+++ Middlewares/Admin.middleware.js
@@ -1,6 +1,6 @@
-module.exports = function adminAuth(req, res, next){
-    // 401 means unAthorized
-    //403 means forbidden
-    if(!req.user.isAdmin) return res.status(403).send("access denied because you are not admin..");
-    next();
-}  
+// module.exports = function adminAuth(req, res, next){
+//     // 401 means unAthorized
+//     //403 means forbidden
+//     if(!req.user.isAdmin) return res.status(403).send("access denied because you are not admin..");
+//     next();
+// }  
