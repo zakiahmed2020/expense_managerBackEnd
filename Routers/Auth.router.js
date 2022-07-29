22a0e50edc2cbf7b92e7c67cebb6b0be77dc14bb
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-07-29 16:38:49 +0300

    Routers - Auth

--- Routers/Auth.router.js
+++ Routers/Auth.router.js
@@ -1,10 +1,14 @@
-const _ = require("lodash");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
-const express = require("express");
+import mongoose from "mongoose";
+import _ from "lodash";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import express from "express";
+import {
+  UserModel,
+  validateUserLogin,
+  validateUserPin,
+} from "../Models/Users.model.js";
 const router = express.Router();
-const { UserModel, validateUserLogin } = require("../Models/Users.model");
 
 router.post("/login", async (req, res) => {
   const { error } = validateUserLogin(req.body);
@@ -32,4 +36,25 @@ router.post("/login", async (req, res) => {
   }
 });
 
-module.exports = router;
+router.post("/loginWithPin", async (req, res) => {
+  const { error } = validateUserPin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const userInfo = await UserModel.findOne({ pin_Number: req.body.pin_Number });
+    if (!userInfo) return res.status(406).send({ message: "Invalid Pin" });
+    // Generate Token and pass though header
+    const token = await userInfo.generateAuthToken();
+    res.header("authorization", token).json({
+      success: 200,
+      userInfo: userInfo._id,
+      access_token: token,
+    });
+  } catch (error) {
+    res.send({
+      status: 404,
+      message: `Error: ${error}`,
+    });
+  }
+});
+
+export default router;
