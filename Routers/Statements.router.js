7e902ba5069f0fa227c0c9929d50f781e57a541f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-07-29 16:39:10 +0300

    Routers - Statements

--- Routers/Statements.router.js
+++ Routers/Statements.router.js
@@ -1,12 +1,14 @@
-const admin = require("../Middlewares/Admin.middleware");
-const Auth = require("../Middlewares/Auth.middleware");
-const express = require("express");
+import Auth from "../Middlewares/Auth.middleware.js";
+import express from "express";
+import { transModel } from "../Models/Transactions.model.js";
 const router = express.Router();
-// router.use(Auth)
-const {
-  transModel,
-  validateTransections,
-} = require("../Models/Transactions.model");
+
+router.use(Auth);
+
+// Check if the arry item is empty
+function isEmpty(array) {
+  return array.length == 0;
+}
 
 // get user transection by populating user and it transections
 router.get("/:id", async function (req, res) {
@@ -16,7 +18,7 @@ router.get("/:id", async function (req, res) {
     data = await transModel.find({ userID: userID }).populate({
       path: "userID",
       model: "users",
-      select: "-_id name accountNo phone amount email",
+      select: "-_id avatar name accountNo phone amount email",
     });
     let bal = 0;
     data = data.map((item) => {
@@ -28,9 +30,7 @@ router.get("/:id", async function (req, res) {
       return newObject;
     });
 
-    // console.log("from data" + data);
-
-    res.json({ info: data });
+    res.send({ info: data });
     // res.send(info);
   } catch (err) {
     console.log(err);
@@ -44,23 +44,25 @@ router.get("/info/:id", async function (req, res) {
   let balance = await userBalance(userID);
   // object laso celiyay waxa ka mid userIncomes,userExpenses, UserBalance, object distructor
   const { userIncomes, userExpenses, UserBalance } = balance;
-
-  res.json({
+  res.send({
+    status: 200,
+    message: "Successfull",
     userincome: userIncomes,
     userExpense: userExpenses,
     balance: UserBalance,
   });
 });
-// fucntion returns totalUserIncome,totalUserExpense,currentUserBalance
 
+// fucntion returns totalUserIncome,totalUserExpense,currentUserBalance
 let userBalance = async function (userID) {
   // get all user incomes adigo check greynayo useridinu jiro iyo type kisu yahay expense
   // wuxu soo celinya array oo ey ku jiran objects like [{...},{...}]
-  const userIncome = await transModel.find({ userID: userID, type: "income" });
+  const userIncome = await transModel.find({ userID: userID, type: "Income" });
   const userExpense = await transModel.find({
     userID: userID,
-    type: "expense",
+    type: "Expense",
   });
+
   // make total by a user add
   //all user incomes it returns like summary Total:300 like that
   const userTotalIncome = userIncome.reduce(
@@ -80,7 +82,57 @@ let userBalance = async function (userID) {
   return userlistInfo;
 };
 
-module.exports = {
-  router: router,
-  userBalance: userBalance,
+// Calculate user Income using map function
+let userIncome = async (userID) => {
+  const userIncome = await transModel.find({ userID: userID, type: "Income" });
+  const userTotalIncome = userIncome.map((user) => user.amount);
+  return userTotalIncome;
 };
+
+// user Income 
+router.get("/userIncome/:id", async function (req, res) {
+  const userID = req.params.id;
+  const user_Income = await userIncome(userID);
+  if (isEmpty(user_Expense)) {
+    res.send({
+      status: 200,
+      userIncome: 0,
+    });
+  } else {
+    res.send({
+      status: 200,
+      message: "Successfull",
+      userIncome: user_Income,
+    });
+  }
+});
+
+// Calculate user Expense using map function return array of user expense
+let userExpense = async (userID) => {
+  const userExpense = await transModel.find({
+    userID: userID,
+    type: "Expense",
+  });
+  const userTotalExpense = userExpense.map((user) => user.amount);
+  return userTotalExpense;
+};
+
+router.get("/userExpense/:id", async (req, res) => {
+  const userID = req.params.id;
+  const user_Expense = await userExpense(userID);
+  if (isEmpty(user_Expense)) {
+    res.send({
+      status: 200,
+      userExpense: 0,
+    });
+  } else {
+    res.send({
+      status: 200,
+      userExpense: user_Expense,
+    });
+  }
+});
+
+const Statements = { router, userBalance };
+
+export default Statements;
